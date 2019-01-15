01866510384edf1be67c309cf9444e20eb1816ef
bdzzz <gavadinov@gmail.com>
2019-01-15 01:12:50 +0000

    Implement some more opcodes.

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include "src/Chip8.h"
 
 int main() {
-	Chip8 chip8{};
+    Chip8 chip8{};
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
-	for (;;) {
-		chip8.cycle();
-	}
+    for (;;) {
+        chip8.cycle();
+    }
 #pragma clang diagnostic pop
 }
--- src/Chip8.h
+++ src/Chip8.h
@@ -4,44 +4,41 @@
 #include <string>
 
 typedef unsigned char BYTE;
-typedef unsigned short WORD;
 
-const int BYTE_SIZE = 8;
-const int WORD_SIZE = 16;
+typedef unsigned short WORD;
 
 class Chip8 {
 public:
-	Chip8() = default;
+    Chip8() = default;
 
-	~Chip8() = default;
+    ~Chip8() = default;
 
-	bool getDrawFlag();
+    bool getDrawFlag();
 
-	void cycle();
+    void cycle();
 
-	bool loadApplication(std::string filename);
+    bool loadApplication(std::string filename);
 
 private:
-	const WORD START_ADDRESS = 512;
-	WORD pc;                // Program counter
-	WORD opcode;            // Current opcode
-	WORD I;                // Index register
-	WORD sp;                // Stack pointer
+    const WORD START_ADDRESS = 512;
+    WORD pc;                // Program counter
+    WORD opcode;            // Current opcode
+    WORD I;                // Index register
+    WORD sp;                // Stack pointer
 
-	BYTE V[16];            // V-registers (V0-VF)
-	WORD stack[16];        // Stack (16 levels)
-	BYTE memory[4096];    // Memory (size = 4k)
+    BYTE V[16];            // V-registers (V0-VF)
+    WORD stack[16];        // Stack (16 levels)
+    BYTE memory[4096];    // Memory (size = 4k)
 
-	BYTE delay_timer;        // Delay timer
-	BYTE sound_timer;        // Sound timer
+    BYTE delay_timer;        // Delay timer
+    BYTE sound_timer;        // Sound timer
 
-	BYTE screen[64*32];    // Total amount of pixels: 2048
-	BYTE key[16];
+    BYTE screen[64 * 32];    // Total amount of pixels: 2048
+    BYTE key[16];
 
-	bool drawFlag;
+    bool drawFlag;
 
-	void init();
-
+    void init();
 
 };
 
